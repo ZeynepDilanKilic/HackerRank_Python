6f302b5f897c2c2d653acfd1d2fd88b4fcc62353
ZeynepDilanKilic <87665057+ZeynepDilanKilic@users.noreply.github.com>
2022-02-28 00:24:29 +0300

    The Captain's Room

---
+++ The Captain's Room.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+s = map(int, input().split())
+s = sorted(s)
+
+for i in range(len(s)):
+    if(i != len(s)-1):
+        if(s[i]!=s[i-1] and s[i]!=s[i+1]):
+            print(s[i])
+            break;
+    else:
+        print(s[i])
