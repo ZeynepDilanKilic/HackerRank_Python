1f87caf84592daccb09913b830a49a64b116c237
ZeynepDilanKilic <87665057+ZeynepDilanKilic@users.noreply.github.com>
2022-04-04 21:45:54 +0300

    Exceptions

---
+++ Exceptions.py
@@ -0,0 +1,11 @@
+n =  int(input())
+
+for i in range(n):
+    a,b =  input().split()
+    
+    try:
+        print(int(a)//int(b))
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+    except ValueError as v:
+        print("Error Code:",v)
